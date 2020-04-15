406dc46fe7818d164750a2c81783bb3c6c603dc3
KaranbirSingh118 <50712392+KaranbirSingh118@users.noreply.github.com>
2020-04-14 23:33:23 -0400

    Add files via upload

---
+++ assets/data/loginInfo.ts
@@ -0,0 +1,9 @@
+import { loginDetails } from '../../app/loginDetails';
+
+export const MYCLASSES: loginDetails[] = [
+    {sname: 'Karanbir Singh',
+    sid: '99155003',
+    scampus: 'Davis',
+    loginName: 'sing9206',
+    assignment: 'Assignment 4',}
+];
